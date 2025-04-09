458f2c211cd9f85f793bcbb08a67aad168d0aff5
adib <adib@pocketdata.com.my>
2025-04-09 20:11:05 +0800

    force udp

--- test_backup_fingerprint.py
+++ test_backup_fingerprint.py
@@ -11,7 +11,7 @@ def backup_device(ip, uid):
 
     try:
         try:
-            conn = ZK(ip, port=4370, timeout=120, password=0, force_udp=False, ommit_ping=False, verbose=True)
+            conn = ZK(ip, port=4370, timeout=120, password=0, force_udp=true, ommit_ping=False, verbose=True)
             conn.connect()
             conn.disable_device()
         except Exception as e:
